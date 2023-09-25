35ff719177486f5f2ea5b754955e799fb3de7847
Niraj Khatri <nirajkhatri0601@gmail.com>
2023-09-25 14:42:45 -0400

    NotesView constructor and event handlers

---
+++ JS/NotesView.js
@@ -0,0 +1,46 @@
+export default class NotesView {
+    // Constructor initializes the view and event handlers
+    constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}) {
+        this.root = root;
+        this.onNoteSelect = onNoteSelect;
+        this.onNoteAdd = onNoteAdd;
+        this.onNoteEdit = onNoteEdit;
+        this.onNoteDelete = onNoteDelete;
+
+        // Create the HTML structure for the notes view
+        this.root.innerHTML = `
+            <div class="notes__sidebar">
+                <button class="notes__add" type="button">Add Note</button>
+                <div class="notes__list"></div>
+            </div>
+            <div class="notes__preview">
+                <input class="notes__title" type="text" placeholder="New Note...">
+                <textarea class="notes__body">Take Note...</textarea>
+            </div>
+        `;
+
+        // Attach event listeners to the Add Note button and input fields
+        const btnAddNote = this.root.querySelector(".notes__add");
+        const inpTitle = this.root.querySelector(".notes__title");
+        const inpBody = this.root.querySelector(".notes__body");
+
+        btnAddNote.addEventListener("click", () => {
+            this.onNoteAdd();
+        });
+
+        [inpTitle, inpBody].forEach(inputField => {
+            inputField.addEventListener("blur", () => {
+                // Extract and send updated title and body to provided event handler
+                const updatedTitle = inpTitle.value.trim();
+                const updatedBody = inpBody.value.trim();
+
+                this.onNoteEdit(updatedTitle, updatedBody);
+            });
+        });
+
+        // Initialize note preview as hidden
+        this.updateNotePreviewVisibility(false);
+    }
+
+    
+}
