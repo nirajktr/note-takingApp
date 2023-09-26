a2f227c802e56e7894602ba09398688fe3c7e828
Niraj Khatri <nirajkhatri0601@gmail.com>
2023-09-25 22:37:24 -0400

    Refactored App class and handlers

--- JS/App.js
+++ JS/App.js
@@ -42,4 +42,45 @@ export default class App {
         this.view.updateActiveNote(note);
     }
 
+    // Function to define event handlers
+    _handlers() {
+        return {
+            onNoteSelect: noteId => {
+                // Find the selected note based on its ID
+                const selectedNote = this.notes.find(note => note.id == noteId);
+
+                // Set the selected note as the active note
+                this._setActiveNote(selectedNote);
+            },
+            // Event handler for adding new note
+            onNoteAdd: () => {
+                const newNote = {
+                    title: "Enter Title...",
+                    body: "Take notes here..."
+                };
+
+                // Save the new note using the API and refresh the notes
+                NotesAPI.saveNote(newNote);
+                this._refreshNotes();
+            },
+            // Event handler for editing a note
+            onNoteEdit: (title, body) => {
+                NotesAPI.saveNote({
+                    id: this.activeNote.id,
+                    title,
+                    body
+                });
+
+                // Refresh the notes
+                this._refreshNotes();
+            },
+            // Event handler for deleting a note
+            onNoteDelete: noteId => {
+
+                // Delete the selected note using the API and refresh the notes
+                NotesAPI.deleteNote(noteId);
+                this._refreshNotes();
+            },
+        };
+    }
 }
